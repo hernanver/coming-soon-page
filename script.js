749e1a4a0ddcc1222c00992309ffff21d11f7dbf
Hernan Verzini <hernanverzini@hotmail.com>
2022-10-13 05:19:24 -0300

    Cambios en js

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const form = document.getElementById('form');
-const email = document.getElementById('email');
+
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    const emailValue = form['email'].value;
+    const email = form['email'];
+    const emailValue = email.value;
+    const small = form.querySelector('small');
 
     if (!emailValue){
-
+        email.classList.add('error');
+        small.innerText = 'Email field cannot be blank!';
+        small.style.display = 'inline-block';
     }
     else if (!isValidEmail(emailValue)) {
-
+        email.classList.add('error');
+        small.innerText = 'Email is invalid!';
+        small.style.display = 'inline-block';
     } else {
-
+        email.classList.remove('error');
+        small.innerText = '';
+        small.style.display = 'none';
     }
 }) ;
+
 function isValidEmail(email) 
 {
  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
   {
     return (true)
   }
-    alert("You have entered an invalid email address!")
+    alert("Please provide a valid email address!")
     return (false)
 }
